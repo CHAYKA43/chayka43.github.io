db0dff941d2b8be747e534f503cf531b9cff1f47
CHAYKA43 <makar.lososev@gmail.com>
2025-08-08 19:29:14 +0000

    какашке5

--- assets/js/geoblock.js
+++ assets/js/geoblock.js
@@ -9,20 +9,21 @@ const blockedRedirectUrl = '/denied/your-country-is-not-supported';
 // Функция для получения геоданных по IP
 async function getGeoLocation() {
     try {
-        const response = await fetch('https://ipapi.co/json/');
+        const response = await fetch('https://api.ip.sb/geoip');
         if (!response.ok) throw new Error('Не удалось получить данные о местоположении');
 
         const data = await response.json();
         const userCountryCode = data.country_code;
 
         if (!allowedCountries.includes(userCountryCode)) {
-            console.log(userCountryCode)
+            window.location.replace(blockedRedirectUrl);
         }
     } catch (error) {
         console.warn('Ошибка при определении страны:', error);
         // При ошибке можно либо пропустить, либо перенаправить — зависит от политики
         // Например, можно перенаправить всех при ошибках (строгий режим), или наоборот — пропустить
-        // Здесь: перенаправляем при ошибках (для безопасности)\\\\\\\\\\
+        // Здесь: перенаправляем при ошибках (для безопасности)
+        window.location.replace(blockedRedirectUrl);
     }
 }
 
