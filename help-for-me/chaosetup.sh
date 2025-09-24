f97a480c4f929a42ee748c5e0836be2dbe2490c6
CHAYKA43 <chayka43@inbox.ru>
2025-09-24 22:59:40 +0300

    Delete help-for-me/chaosetup.sh

--- help-for-me/chaosetup.sh
+++
@@ -1,8 +0,0 @@
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
-sleep 1
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-sleep 1
-echo -n "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee /etc/pacman.conf
-sudo pacman -Syyu
